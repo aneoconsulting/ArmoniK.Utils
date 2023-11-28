1e9abec3587c7f69fd9b6e2af8675f9cdf2c0131
Florian Lemaitre <flemaitre@aneo.fr>
2023-11-28 14:22:45 +0100

    Add ViewSelect extension

---
+++ csharp/src/CollectionExt.cs
@@ -0,0 +1,39 @@
+﻿// This file is part of the ArmoniK project
+//
+// Copyright (C) ANEO, 2022-2023.All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License")
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System;
+using System.Collections.Generic;
+
+using JetBrains.Annotations;
+
+namespace ArmoniK.Utils;
+
+public static class CollectionExt
+{
+  /// <summary>
+  ///   Creates a view over a collection
+  /// </summary>
+  /// <param name="collection"></param>
+  /// <param name="projection"></param>
+  /// <typeparam name="Tsrc"></typeparam>
+  /// <typeparam name="Tdst"></typeparam>
+  /// <returns></returns>
+  [PublicAPI]
+  public static CollectionView<Tsrc, Tdst> ViewSelect<Tsrc, Tdst>(this ICollection<Tsrc> collection,
+                                                                  Func<Tsrc, Tdst>       projection)
+    => new(collection,
+           projection);
+}
---
+++ csharp/src/CollectionView.cs
@@ -0,0 +1,101 @@
+// This file is part of the ArmoniK project
+//
+// Copyright (C) ANEO, 2022-2023.All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License")
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+using JetBrains.Annotations;
+
+namespace ArmoniK.Utils;
+
+/// <summary>
+///   View over a collection of <typeparamref name="Tsrc" />.
+///   All elements are projected into <typeparamref name="Tdst" />.
+/// </summary>
+/// <typeparam name="Tsrc">Type of the underlying collection elements</typeparam>
+/// <typeparam name="Tdst">Type of the projected elements</typeparam>
+/// <remarks>
+///   If the underlying is modified, the view will reflect the changes.
+///   If it is safe to iterate over the underlying collection while modifying it,
+///   it is safe to iterate over the view while modifying the underlying collection.
+/// </remarks>
+[PublicAPI]
+public class CollectionView<Tsrc, Tdst> : ICollection<Tdst>
+{
+  private readonly ICollection<Tsrc> collection_;
+  private readonly Func<Tsrc, Tdst>  projection_;
+
+  /// <summary>
+  ///   Project <paramref name="collection" /> into a view
+  /// </summary>
+  /// <param name="collection">Collection to project</param>
+  /// <param name="projection">
+  ///   Function to project <typeparamref name="Tsrc" /> elements into <typeparamref name="Tdst" />
+  ///   elements
+  /// </param>
+  [PublicAPI]
+  public CollectionView(ICollection<Tsrc> collection,
+                        Func<Tsrc, Tdst>  projection)
+  {
+    projection_ = projection;
+    collection_ = collection;
+  }
+
+  /// <inheritdoc />
+  public IEnumerator<Tdst> GetEnumerator()
+    => collection_.Select(projection_)
+                  .GetEnumerator();
+
+  /// <inheritdoc />
+  IEnumerator IEnumerable.GetEnumerator()
+    => GetEnumerator();
+
+  /// <inheritdoc />
+  public void Add(Tdst item)
+    => throw new NotSupportedException("View Collection is read-only");
+
+  /// <inheritdoc />
+  public void Clear()
+    => throw new NotSupportedException("View Collection is read-only");
+
+  /// <inheritdoc />
+  public bool Contains(Tdst item)
+    => throw new NotSupportedException("View Collection is read-only");
+
+  /// <inheritdoc />
+  public void CopyTo(Tdst[] array,
+                     int    arrayIndex)
+  {
+    foreach (var x in this)
+    {
+      array[arrayIndex++] = x;
+    }
+  }
+
+  /// <inheritdoc />
+  public bool Remove(Tdst item)
+    => throw new NotSupportedException("View Collection is read-only");
+
+  /// <inheritdoc />
+  public int Count
+    => collection_.Count;
+
+  /// <inheritdoc />
+  public bool IsReadOnly
+    => true;
+}
