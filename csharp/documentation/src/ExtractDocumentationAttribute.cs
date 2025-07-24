8d3ffc01b9cc3ba771b8e1d4949c7a79d5d012d6
José Fonseca <jfonseca@aneo.fr>
2025-07-24 14:39:49 +0200

    chore: add document extraction tool

---
+++ csharp/documentation/src/ExtractDocumentationAttribute.cs
@@ -0,0 +1,37 @@
+// This file is part of the ArmoniK project
+//
+// Copyright (C) ANEO, 2022-$CURRENT_YEAR.All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License")
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+namespace ArmoniK.Utils.DocExtractor;
+
+/// <summary>
+///   Indicates that a class should have its property documentation collected.
+/// </summary>
+[AttributeUsage(AttributeTargets.Class,
+                Inherited = false)]
+public class ExtractDocumentationAttribute : Attribute
+{
+  /// <summary>
+  ///   Initializes a new instance of the <see cref="ExtractDocumentationAttribute" /> class.
+  /// </summary>
+  /// <param name="description">An optional description for the attribute, providing context about the class.</param>
+  public ExtractDocumentationAttribute(string description = "")
+    => Description = description;
+
+  /// <summary>
+  ///   Gets the description of the attribute.
+  /// </summary>
+  public string Description { get; }
+}
---
+++ csharp/documentation/src/Program.cs
@@ -0,0 +1,50 @@
+// This file is part of the ArmoniK project
+//
+// Copyright (C) ANEO, 2022-2023.All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License")
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+namespace ArmoniK.Utils.DocExtractor;
+
+internal abstract class Program
+{
+  private static async Task Main(string[] args)
+  {
+    if (args.Length == 0)
+    {
+      Console.WriteLine("Please provide the path to the solution file.");
+      return;
+    }
+
+    var solutionPath = args[0];
+
+    if (!File.Exists(solutionPath))
+    {
+      Console.WriteLine($"Given solution file {solutionPath} does not exist.");
+      return;
+    }
+
+    var markdown  = await MarkdownDocGenerator.GenerateAsync(solutionPath);
+
+    if (markdown is null)
+    {
+      Console.WriteLine($"Failed to load solution from path: {solutionPath}");
+      return;
+    }
+
+    var outputFileName = Path.GetFileNameWithoutExtension(solutionPath) + ".EnvVars.md";
+    await File.WriteAllTextAsync(outputFileName,
+                                 markdown);
+    Console.WriteLine($"Markdown file generated: {outputFileName}");
+  }
+}
