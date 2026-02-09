0bb19899c23f959459711a881ba5a93dcfcef2fe
Florian Lemaitre <flemaitre@aneo.fr>
2026-02-09 14:09:43 +0100

    Reformat everything

--- csharp/tests/ChunkTest.cs
+++ csharp/tests/ChunkTest.cs
@@ -1,13 +1,13 @@
 ﻿// This file is part of the ArmoniK project
-//
-// Copyright (C) ANEO, 2022-2023.All rights reserved.
-//
+// 
+// Copyright (C) ANEO, 2022-2026. All rights reserved.
+// 
 // Licensed under the Apache License, Version 2.0 (the "License")
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
+// 
 //     http://www.apache.org/licenses/LICENSE-2.0
-//
+// 
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -41,9 +41,9 @@ public class ChunkTest
     }
   }
 
-  ///////////////////////
+  // ///////////////// //
   // Synchronous Chunk //
-  ///////////////////////
+  // ///////////////// //
   [Test]
   [TestCaseSource(nameof(ChunkArrayCases),
                   new object[]
@@ -145,9 +145,9 @@ public class ChunkTest
                 Throws.TypeOf<ArgumentOutOfRangeException>());
   }
 
-  ////////////////////////
+  // ////////////////// //
   // Asynchronous Chunk //
-  ////////////////////////
+  // ////////////////// //
   [Test]
   [AbortAfter(1000)]
   [TestCaseSource(nameof(ChunkArrayCases),
@@ -263,9 +263,9 @@ public class ChunkTest
   }
 
 
-  ///////////////////////////////////////
+  // ///////////////////////////////// //
   // Asynchronous Chunk specific tests //
-  ///////////////////////////////////////
+  // ///////////////////////////////// //
   [Test]
   [AbortAfter(10000)]
   public async Task ChunkAsyncWithDelay()
--- csharp/tests/DeferTest.cs
+++ csharp/tests/DeferTest.cs
@@ -1,13 +1,13 @@
 // This file is part of the ArmoniK project
-//
-// Copyright (C) ANEO, 2022-2023.All rights reserved.
-//
+// 
+// Copyright (C) ANEO, 2022-2026. All rights reserved.
+// 
 // Licensed under the Apache License, Version 2.0 (the "License")
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
+// 
 //     http://www.apache.org/licenses/LICENSE-2.0
-//
+// 
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -32,9 +32,9 @@ public class DeferTest
     AsyncDisposable,
   }
 
-  ////////////////////////////
+  // ////////////////////// //
   // Synchronous Disposable //
-  ////////////////////////////
+  // ////////////////////// //
   [Test]
   [AbortAfter(1000)]
   public void DeferEmpty()
@@ -225,9 +225,9 @@ public class DeferTest
                 Is.EqualTo(2));
   }
 
-  /////////////////////////////
+  // /////////////////////// //
   // Asynchronous Disposable //
-  /////////////////////////////
+  // /////////////////////// //
   [Test]
   [AbortAfter(1000)]
   public async Task AsyncDeferEmpty()
@@ -521,9 +521,9 @@ public class DeferTest
                       delay,
                       f);
 
-  ///////////
+  // ///// //
   // Utils //
-  ///////////
+  // ///// //
   private record DisposableWrapper(IDisposable Disposable) : IDisposable
   {
     public void Dispose()
